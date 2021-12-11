4ae80c12bfc1ce0713ba1cacc762445ee223859a
Vivian Yeh <u4h93u6uhu@gmail.com>
2021-12-11 12:31:14 +0800

    feat: add ESLint

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-  <h1 class="text-[100px]">
-    It must be good!
-  </h1>
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+    <h1 class="text-[100px]">
+        It must be good!
+    </h1>
+    <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
 <style>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import eslintPlugin from 'vite-plugin-eslint';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), eslintPlugin()],
   base: "/Vue3-vite-tailwind-jit-mode/"
 })
